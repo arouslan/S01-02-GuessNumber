d47198a369461ae6e366e9a07ad76f9c5c8826bf
arouslan <rouslan@inbox.ru>
2013-04-17 22:36:14 +0700

    Первый вариант игры угадай число

---
+++ src/com/abushaev/kurs/s0102/guessnumber/GNumber.java
@@ -0,0 +1,19 @@
+package com.abushaev.kurs.s0102.guessnumber;
+
+import com.abushaev.kurs.s0102.guessnumber.controllers.GController;
+import com.abushaev.kurs.s0102.guessnumber.views.GConsoleUI;
+
+/**
+ * Author: Abushaev Ruslan
+ * e-mail: rouslan@inbox.ru
+ * Date: 17.04.13
+ * Time: 10:18
+ */
+public class GNumber {
+
+    public static void main(String[] args) {
+        GConsoleUI gUIConsole = new GConsoleUI();
+        GController gController = new GController(gUIConsole);
+        gController.Run();
+    }
+}
